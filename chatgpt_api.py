cacba9419f8e984fb7b57b1bdc4f8f8453a67d5b
akorson <aaron.korson@chiattorney.com>
2023-04-29 16:56:52 -0500

    update to chatgptapi

--- chatgpt_api.py
+++ chatgpt_api.py
@@ -1,8 +1,8 @@
 import os
-
 import openai
-import pymongo
+import mongoengine
 from dotenv import load_dotenv
+from mongoengine import Document, StringField, connect
 
 load_dotenv()
 
@@ -11,21 +11,53 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # connect to MongoDB
 MONGO_URI = os.environ["MONGO_URI"]
-client = pymongo.MongoClient(MONGO_URI)
-db = client.chatgpt
-memory_collection = db.memory
+connect(host=MONGO_URI)
 
+# define memory classes
+class ShortTermMemory(Document):
+    prompt = StringField(required=True)
+    response = StringField(required=True)
+
+
+class LongTermMemory(Document):
+    prompt = StringField(required=True)
+    response = StringField(required=True)
+
+# file handling functions
+def save_file(file_name, content):
+    with open(file_name, "w") as f:
+        f.write(content)
+
+def read_file(file_name):
+    with open(file_name, "r") as f:
+        return f.read()
+
+def delete_file(file_name):
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+    else:
+        print(f"Error: {file_name} not found")
 
 # add short-term memory to the prompt
 def add_short_term_memory(prompt, response):
+    stm = ShortTermMemory(prompt=prompt, response=response)
+    stm.save()
     return f"{prompt}\n{response}\n"
-
 
 # insert a single document into the MongoDB collection
 def add_long_term_memory(prompt, response):
-    memory = {"prompt": prompt, "response": response}
-    memory_collection.insert_one(memory)
+    ltm = LongTermMemory(prompt=prompt, response=response)
+    ltm.save()
 
+# recall short-term memory
+def recall_short_term_memory(prompt):
+    stm = ShortTermMemory.objects(prompt=prompt).first()
+    return stm.response if stm else None
+
+# recall long-term memory
+def recall_long_term_memory(prompt):
+    ltm = LongTermMemory.objects(prompt=prompt).first()
+    return ltm.response if ltm else None
 
 # get a response from OpenAI API
 def get_response(
@@ -59,7 +91,6 @@ def get_response(
     )
 
     return response.choices[0].text.strip()
-
 
 # insert a long-term memory into the MongoDB collection
 def remember(prompt, response):
