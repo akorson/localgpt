9ce206b43b2731be53ef34a562e0e2f707e32963
akorson <aaron.korson@chiattorney.com>
2023-04-30 12:47:26 -0500

    corrections

---
+++ search_engines.py
@@ -0,0 +1,26 @@
+# search_engines.py
+import os
+import googleapiclient.discovery
+from google.oauth2 import service_account
+from bing_search_api_v7 import BingSearch
+from github import Github
+
+def google_search(query):
+    api_key = os.environ["GOOGLE_API_KEY"]
+    cse_id = os.environ["GOOGLE_CSE_ID"]
+    credentials = service_account.Credentials.from_service_account_info(api_key)
+    service = googleapiclient.discovery.build("customsearch", "v1", credentials=credentials)
+    result = service.cse().list(q=query, cx=cse_id).execute()
+    return result["items"]
+
+def bing_search(query):
+    api_key = os.environ["BING_API_KEY"]
+    bing = BingSearch(api_key)
+    results = bing.search(query)
+    return results["webPages"]["value"]
+
+def github_search(query):
+    access_token = os.environ["GITHUB_ACCESS_TOKEN"]
+    github = Github(access_token)
+    results = github.search_repositories(query)
+    return results
